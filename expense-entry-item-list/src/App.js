2240964ad92a692d16fbb237ba836f75745bf6db
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-02 00:53:34 +0300

    Final Update

--- expense-entry-item-list/src/App.js
+++ expense-entry-item-list/src/App.js
@@ -5,7 +5,11 @@ import ExpenseEntryItemList from './Components/ExpenseEntryItemList';
 function App() {
   return (
     <div className="App">
+      <>
+      <h1 style={{textAlign: "center"}}>Expense Entry Item List</h1>
       <ExpenseEntryItemList />
+      </>
+      
     </div>
   );
 }
